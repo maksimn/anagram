e01614c6fcf4db996c9340baba9c6ec78192d21d
maksimn <maksim1989sovunion@yandex.ru>
2016-09-25 02:11:31 +0300

    Класс Anagram и его первый тест

---
+++ Anagram.Tests/AnagramModel/AnagramTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+using AnagramModel;
+
+[TestFixture]
+public class AnagramTests {
+    [Test]
+    public void GetAnagramClass_GetAnagramClass_ReturnsCorrectClass() {
+        Anagram anagram = new Anagram("word");
+
+        Assert.AreEqual("dorw", anagram.Class);
+    }
+}
