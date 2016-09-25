9bddc88bb6fd88bf6b26fd22c83f1fcd3407ad85
maksimn <maksim1989sovunion@yandex.ru>
2016-09-25 08:25:21 +0300

    Переименование класса Anagram --> Word и рефакторинг кода

---
+++ Anagram.Tests/AnagramModel/WordTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+using AnagramModel;
+
+[TestFixture]
+public class WordTests {
+    [TestCase("word", "dorw")]
+    [TestCase("ток", "кот")]
+    public void AnagramClass_GetAnagramClass_ReturnsCorrectClass(String str, String expected) {
+        Word word = new Word(str);
+
+        Assert.AreEqual(expected, word.AnagramClass);
+    }
+}
---
+++ Anagram/AnagramModel/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnagramModel {
+    public class Word {
+        private String value;
+
+        public Word(String word) {
+            value = word;
+        }
+
+        public String AnagramClass {
+            get {
+                String lower = value.ToLower();
+                Char[] chars = lower.ToCharArray();
+                Array.Sort(chars);
+                return new String(chars);
+            }
+        }
+    }
+}
